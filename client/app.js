a290caa41007df03adee8a5374896d7e3d48f007
rishiraj Bhowmik <rishiraj.bhowmik5@gmail.com>
2025-12-07 16:47:31 +0530

    Using Socket.io

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-const socket=new WebSocket("ws://localhost:3000");
+const socket=io("ws://localhost:3000");
 
 
 const form=document.querySelector("form");
@@ -7,14 +7,14 @@ function sendMessage(e){
     e.preventDefault();
     const input=document.querySelector("input");
     if(input.value){
-        socket.send(input.value)
+        socket.emit("message",input.value)
         input.value="";
     }
 }
 
 form.addEventListener("submit",sendMessage);
 
-socket.addEventListener("message",({data})=>{
+socket.on("message",(data)=>{
     console.log("Message from server ",data)
     const ul =document.querySelector("ul");
     const li=document.createElement("li");
