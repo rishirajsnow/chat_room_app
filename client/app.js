ed9c5c6f97e70383d535a99e981ad6ccbd3b74aa
rishiraj Bhowmik <rishiraj.bhowmik5@gmail.com>
2025-12-07 18:00:59 +0530

    Final functionality added.

--- client/app.js
+++ client/app.js
@@ -2,6 +2,8 @@ const socket=io("ws://localhost:3000");
 
 
 const form=document.querySelector("form");
+const p=document.querySelector("p");
+const input=document.querySelector("input");
 
 function sendMessage(e){
     e.preventDefault();
@@ -20,4 +22,18 @@ socket.on("message",(data)=>{
     const li=document.createElement("li");
     li.textContent=data;
     ul.appendChild(li);
+})
+
+input.addEventListener("keypress",()=>{
+    socket.emit("activity",`${socket.id}`);
+})
+let timer;
+socket.on("activity",(data)=>{
+    clearTimeout(timer)
+    
+    p.innerText=`${data.substring(0,5)} is typing ...`;
+    timer=setTimeout(()=>{
+        p.innerText=""
+    },3000)
+
 })
--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
-const http=require("http");
-const {Server}=require("socket.io")
+import express from "express";
+import {Server} from "socket.io";
+const app=express();
 
-const httpserver=http.createServer()
+const httpServer=app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
 
-httpserver.listen(3000,()=>console.log("Http server started"))
-// const wsServer= new ws.WebSocketServer({port:3000});
-const io=new Server(httpserver,{
+
+const io=new Server(httpServer,{
     cors:{
         origin:"*"
     }
@@ -13,6 +15,24 @@ const io=new Server(httpserver,{
 
 io.on("connection",(socket)=>{
     console.log("Websocket connection established",socket.id)
+
+    //Only to the current user
+    socket.emit("message",`Welcome to the room ${socket.id.substring(0,5)}`)
+
+    //To all users except the current user
+    socket.broadcast.emit("message",`A new user has joined the room :${socket.id.substring(0,5)}`)
+
+    //To all users including the current user
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit("message",`User disconnected: ${socket.id}`)
+    })
+
+    //User activity tracker
+
+    socket.on("activity",(name)=>{
+        socket.broadcast.emit("activity",`${name}`)
+    })
+
     socket.on("message",(data)=>{
         io.emit("message",`${socket.id.substring(0,5)} : ${data}`)
     })
